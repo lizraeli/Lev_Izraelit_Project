93ec797c45831873c6933932ec393124c463cdf3
lizraeli <leo2002b@yahoo.com>
2023-04-11 23:12:39 -0400

    update requests

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -1,6 +1,6 @@
+import type { Movie, Quote, Character } from 'src/models';
 import { fetchWithAuth } from './fetch';
 import * as urls from './urls';
-import type { Movie } from 'src/models/Movie';
 
 interface MoviesResponse {
   docs: Movie[];
@@ -9,5 +9,39 @@ interface MoviesResponse {
 export const getMovies = async (): Promise<Movie[]> => {
   const res = await fetchWithAuth(urls.movies);
   const data = (await res.json()) as MoviesResponse;
+
+  return data.docs;
+};
+
+interface MovieResponse {
+  docs: Movie[];
+}
+
+export const getMovie = async (movieUuid: string): Promise<Movie> => {
+  const res = await fetchWithAuth(urls.movie(movieUuid));
+  const data = (await res.json()) as MovieResponse;
+
+  return data.docs[0];
+};
+
+interface MovieQuotesResponse {
+  docs: Quote[];
+}
+
+export const getMovieQuotes = async (movieUuid: string): Promise<Quote[]> => {
+  const res = await fetchWithAuth(urls.movieQuotes(movieUuid));
+  const data = (await res.json()) as MovieQuotesResponse;
+
+  return data.docs;
+};
+
+interface CharacterResponse {
+  docs: Character[];
+}
+
+export const getCharacters = async (): Promise<Character[]> => {
+  const res = await fetchWithAuth(urls.characters);
+  const data = (await res.json()) as CharacterResponse;
+
   return data.docs;
 };
--- src/api/urls.ts
+++ src/api/urls.ts
@@ -1,3 +1,7 @@
 const baseUrl = 'https://the-one-api.dev/v2';
 
-export const movies = `${baseUrl}/movie`;
+export const movies = `${baseUrl}/movie?sort=_id:desc`;
+export const movie = (uuid: string) => `${baseUrl}/movie/${uuid}`;
+export const movieQuotes = (uuid: string) => `${baseUrl}/movie/${uuid}/quote`;
+
+export const characters = `${baseUrl}/character`;
