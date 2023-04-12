a01ccfdf27deb23b31c237033a979aab9e1366c6
lizraeli <leo2002b@yahoo.com>
2023-04-11 23:12:24 -0400

    update error handling

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -1,6 +1,17 @@
 const API_KEY = process.env.API_KEY;
 
-export const fetchWithAuth = (url: string) =>
-  fetch(url, {
-    headers: { Authorization: `Bearer ${API_KEY}`, 'User-Agent': '*' },
+export const fetchWithAuth = async (url: string): Promise<Response> => {
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      Accept: 'application/json, text/plain, */*',
+      'User-Agent': '*',
+    },
   });
+
+  if (response.status <= 400) {
+    throw new Error(response.statusText);
+  }
+
+  return response;
+};
