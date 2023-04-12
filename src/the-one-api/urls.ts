0025ae990aa339eb3cc8acdb43d5d5725f8a6038
lizraeli <leo2002b@yahoo.com>
2023-04-12 10:46:27 -0400

    sort characters by name

--- src/the-one-api/urls.ts
+++ src/the-one-api/urls.ts
@@ -4,7 +4,7 @@ export const movies = () => `${baseUrl}/movie?sort=_id:desc`;
 export const movie = (id: string) => `${baseUrl}/movie/${id}`;
 export const movieQuotes = (id: string) => `${baseUrl}/movie/${id}/quote`;
 
-export const characters = () => `${baseUrl}/character`;
+export const characters = () => `${baseUrl}/character?sort=name:asc`;
 export const character = (id: string) => `${baseUrl}/character/${id}`;
 export const characterQuotes = (id: string) =>
   `${baseUrl}/character/${id}/quote`;
